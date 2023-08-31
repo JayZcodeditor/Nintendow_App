35e7e54b04c186e596f7b642f4610532b3ff88a3
JayZ <110582144+JayZcodeditor@users.noreply.github.com>
2023-08-31 15:26:20 +0700

    update

--- lib/models/config.dart
+++ lib/models/config.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_application_1/models/user.dart';
 
 class Configure {
-  static const server = "10.200.6.46:3000";
+  static const server = "10.116.2.86:3000";
   static Users login = Users();
   static List<String> gender = [
     "None",
